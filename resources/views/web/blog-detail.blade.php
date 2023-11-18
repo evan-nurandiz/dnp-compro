12f681e6aca01b697ea594df9997e40d19c364f7
evan nurandiz <muhammad.nurandiz@jubelio.com>
2023-11-18 21:14:13 +0700

    fix code

--- resources/views/web/blog-detail.blade.php
+++ resources/views/web/blog-detail.blade.php
@@ -11,16 +11,16 @@
         <div class="flex justify-between mb-10">
             <p class="text-[14px] text-[#5f5e5e] text-extraLight">All Posts</p>
         </div>
-        <div class="border-[1px] border-gray-100 md:px-24 md:py-16">
+        <div class="border-[1px] p-4 border-gray-100 md:px-24 md:py-16">
             <div class="flex justify-between mb-12 items-center">
                 <div class="flex gap-2 items-center">
-                    <img src="{{url('/icon/name_icon.png')}}" alt="" class="w-[32px] h-[32px] rounded-full">
+                    <img src="{{url('/icon/name_icon.png')}}" alt="" class="w-[24px] h-[24px] md:w-[32px] md:h-[32px] rounded-full">
                     <div class="flex items-center gap-1">
                         <div class="flex items-center">
-                            <p class="text-[#2f2e2e] text-[12px]">Devin GodFrey</p>
+                            <p class="text-[#2f2e2e] text-[10px] md:text-[12px]">Devin GodFrey</p>
                             <img src="{{url('/icon/crown-icon.svg')}}" alt="">
                         </div>
-                        <p class="text-[#2f2e2e] text-[12px]">• {{ \Carbon\Carbon::parse($blog->created_at)->format('M d, Y')}} • {{$blog->read_time}}</p>
+                        <p class="text-[#2f2e2e] text-[10px] md:text-[12px]">• {{ \Carbon\Carbon::parse($blog->created_at)->format('M d, Y')}} • {{$blog->read_time}}</p>
                     </div>
                 </div>
                 <img src="{{url('/icon/blog-more-icon.svg')}}" alt="">
